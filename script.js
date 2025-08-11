3a3b7898b4e40af1f53eed77ac901a72ef2f24e8
RstK <208619198+bowman025@users.noreply.github.com>
2025-08-11 18:50:05 +0200

    Edit game logic slightly and add module to display the game

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const gameboard = (function() {
+const gameBoard = (function() {
     const rows = [1, 2, 3];
     const columns = [1, 2, 3];
-    const token = 0;
+    const token = null;
     const board = [];
     rows.forEach(row => {
             columns.forEach(column => {
@@ -22,12 +22,12 @@ function playerFactory(name, token) {
     return {name, token};
 };
 
-const playerOne = playerFactory("Player 1", 1);
-const playerTwo = playerFactory("Player 2", 2);
+const playerOne = playerFactory("Player 1", "X");
+const playerTwo = playerFactory("Player 2", "O");
 
-const game = (function() {
+const gameController = (function() {
     let activePlayer = playerOne;
-    const cells = gameboard.getState();
+    const cells = gameBoard.getState();
     const switchActivePlayer = () => {
         activePlayer = activePlayer === playerOne ? playerTwo : playerOne;
     };
@@ -38,32 +38,64 @@ const game = (function() {
     const checkWinner = () => {
         let activeCells = cells.filter(cell => cell.token === activePlayer.token);
         console.log(activeCells);
-            if(activeCells.filter(activeCell => activeCell.row === 1).length === 3 ||
-            activeCells.filter(activeCell => activeCell.row === 2).length === 3 ||
-            activeCells.filter(activeCell => activeCell.row === 3).length === 3 ||
-            activeCells.filter(activeCell => activeCell.column === 1).length === 3 ||
-            activeCells.filter(activeCell => activeCell.column === 2).length === 3 ||
-            activeCells.filter(activeCell => activeCell.column === 3).length === 3 ||
-            cells[2].token === activePlayer.token && cells[4].token === activePlayer.token && cells[6].token === activePlayer.token ||
-            cells[0].token === activePlayer.token && cells[4].token === activePlayer.token && cells[8].token === activePlayer.token) {
-                console.log(`${activePlayer.name} wins!`);
-            } else if((cells.filter(cell => cell.token !== 0)).length === 9) {
-                console.log("It's a tie!");
-            } else {
-                switchActivePlayer();
-                printNewRound();
-            };
+        if(activeCells.filter(activeCell => activeCell.row === 1).length === 3 ||
+        activeCells.filter(activeCell => activeCell.row === 2).length === 3 ||
+        activeCells.filter(activeCell => activeCell.row === 3).length === 3 ||
+        activeCells.filter(activeCell => activeCell.column === 1).length === 3 ||
+        activeCells.filter(activeCell => activeCell.column === 2).length === 3 ||
+        activeCells.filter(activeCell => activeCell.column === 3).length === 3 ||
+        cells[2].token === activePlayer.token && cells[4].token === activePlayer.token && cells[6].token === activePlayer.token ||
+        cells[0].token === activePlayer.token && cells[4].token === activePlayer.token && cells[8].token === activePlayer.token) {
+            console.log(`${activePlayer.name} wins!`)
+            return `${activePlayer.name} wins!`;
+        } else if((cells.filter(cell => cell.token !== null)).length === 9) {
+            console.log("It's a tie!");
+            return "It's a tie!";
+        } else {
+            return false;
+        };
     };
     const playRound = (row, col) => {
-        if(gameboard.getCell(row, col).token !== 0) {
+        if(gameBoard.getCell(row, col).token !== null) {
             console.log('That cell is taken. Please pick another one.')
             printNewRound();
             return;
         };
-        gameboard.addToken(activePlayer, row, col);
+        gameBoard.addToken(activePlayer, row, col);
         console.log(`Marking cell row ${row} column ${col} as ${activePlayer.token}`);
-        checkWinner();
+        if (checkWinner() === false) {
+            switchActivePlayer();
+            printNewRound();
         };
+    };
     printNewRound();
-    return {playRound, getActivePlayer};
+    return {playRound, getActivePlayer, checkWinner, printNewRound};
+})();
+
+const displayController = (function() {
+    const boardDiv = document.querySelector(".board");
+    const playerTurnDiv = document.querySelector(".turn");
+    const updateScreen = () => {
+        const cells = gameBoard.getState();
+        let activePlayer = gameController.getActivePlayer();
+        playerTurnDiv.textContent = `It's ${activePlayer.name}'s turn.`
+        cells.forEach((cell, index) => {
+            const cellDiv = document.createElement("div");
+            cellDiv.classList.add("cell", `cell-${index}`);
+            boardDiv.appendChild(cellDiv);
+            cellDiv.addEventListener("click", () => {
+                gameController.playRound(cell.row, cell.column);
+                cellDiv.textContent = cell.token;
+                activePlayer = gameController.getActivePlayer();
+                playerTurnDiv.textContent = `It's ${activePlayer.name}'s turn.`
+                if(gameController.checkWinner() !== false) {
+                    playerTurnDiv.textContent = gameController.checkWinner();
+                    let overlay = document.createElement("div");
+                    overlay.classList.add("overlay");
+                    document.querySelector(".board").appendChild(overlay);
+                };
+            });
+        });
+    };
+    updateScreen();
 })();
